297f36130de41de38a96f23841a66cd8822f2d0b
luvanthanh <luvanthanhby475@gmail.com>
2025-09-17 23:37:20 +0700

    them giao dien trang chu

---
+++ BackEnd/Myproject/Myproject/src/test/java/LuVanThanh/Myproject/MyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package LuVanThanh.Myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
